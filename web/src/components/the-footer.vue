e8fd38ca127de7099255474add94efedf75beaf8
RynDong <92448663+RynDong@users.noreply.github.com>
2023-05-06 20:54:27 +0800

    Original Status

---
+++ web/src/components/the-footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="text-muted">
+    <div class="container">
+      <p class="float-right">
+        <a href="#">回到顶部</a>
+      </p>
+      <p>索拉里斯在线教学平台</p>
+      <p>2099&nbsp;imooc.com&nbsp;&nbsp;京ICP备 1234567890号</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+
+  export default {
+    name: 'theFooter',
+  }
+</script>
